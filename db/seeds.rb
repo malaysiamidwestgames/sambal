17fec809cc2092940701af8c46b5bc7394c9fa15
lidsky <hmr1808@gmail.com>
2015-01-11 19:55:45 -0500

    add user resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: "hasbro@gmail.com")
+User.create(email: "pereira@gmail.com")
