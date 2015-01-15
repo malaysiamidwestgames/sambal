45a09bf89e9a963e5525a684ed9c0633bb088210
lidsky <hmr1808@gmail.com>
2015-01-14 19:20:34 -0500

    add access token field to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150114055425) do
+ActiveRecord::Schema.define(version: 20150115001839) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "access_token"
   end
 
 end
