f820e32720f3235c0cd464413b96d5a0ce2951cc
lidsky <hmr1808@gmail.com>
2015-01-15 22:15:15 -0500

    add sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::API
+  include SessionsHelper
+  include ActionController::Cookies
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-    # The priority is based upon order of creation: first created -> highest priority.
+  # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
@@ -57,6 +57,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     resources :users, except: [:new, :edit]
+    resources :sessions, only: [:create, :destroy]
   end
 
 end
