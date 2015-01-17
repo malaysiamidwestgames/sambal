acf26a141759aa283eb85e57c3d22466da1f9387
lidsky <hmr1808@gmail.com>
2015-01-17 05:12:29 -0500

    add delete path

--- config/routes.rb
+++ config/routes.rb
@@ -58,6 +58,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, except: [:new, :edit]
     resources :sessions, only: [:create, :destroy]
+    delete '/delete_token' => 'sessions#destroy'
   end
 
 end
