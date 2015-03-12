1aedba969c37f561883dcb4761c08f62a8da0a40
lidsky <hmr1808@gmail.com>
2015-03-12 19:09:02 -0400

    add first_name and last_name to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150222033239) do
+ActiveRecord::Schema.define(version: 20150312230612) do
 
   create_table "mailers", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 20150222033239) do
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
 end
