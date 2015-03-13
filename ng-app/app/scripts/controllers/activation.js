0dc81dbe1fe4877098231608e0f4b9f252c4da0a
lidsky <hmr1808@gmail.com>
2015-03-13 18:42:17 -0400

    add ability to retry account activation if failed, todo: refactor

--- ng-app/app/scripts/controllers/activation.js
+++ ng-app/app/scripts/controllers/activation.js
@@ -10,9 +10,13 @@
 
 
 angular.module('midwestApp')
-  .controller('ActivationCtrl', function ($scope, $routeParams, $timeout, $location, httpWrapper) {
+  .controller('ActivationCtrl', function ($scope, $routeParams, $timeout, $location, $http, httpWrapper) {
     $scope.calledApi = false;
     $scope.serverBusy = false;
+    $scope.accountActivation = {
+      visible: false,
+      UserId: null
+    };
     var accountActivationReq = httpWrapper.patch('/api/account_activations/:token/');
 
     accountActivationReq({token: $routeParams.token, email: $routeParams.email})
@@ -24,10 +28,23 @@ angular.module('midwestApp')
         $timeout(function() {
           $location.path('/');
         }, 3000);
-      }, function() {
+      }, function(res) {
         $scope.calledApi = true;
         $scope.isActivated = false;
+        if (res.data.user_id !== undefined) {
+          $scope.accountActivation.visible = true;
+          $scope.accountActivation.UserId = res.data.user_id;
+        }
       });
+
+    $scope.activateAccount = function() {
+      $scope.serverBusy = true;
+      // resend activation email
+      $http.get('/api/users/activations/' + $scope.accountActivation.UserId)
+        .success(function() {
+          $location.path('/confirm_email');
+        });
+    };
 
 
 });
