696b114bdce80ba7ac28b7222b18a06359fb8f4e
lidsky <hmr1808@gmail.com>
2015-03-13 16:51:41 -0400

    change cursor style to progress when waiting server response

--- ng-app/app/scripts/controllers/activation.js
+++ ng-app/app/scripts/controllers/activation.js
@@ -12,12 +12,14 @@
 angular.module('midwestApp')
   .controller('ActivationCtrl', function ($scope, $routeParams, $timeout, $location, httpWrapper) {
     $scope.calledApi = false;
+    $scope.serverBusy = false;
     var accountActivationReq = httpWrapper.patch('/api/account_activations/:token/');
 
     accountActivationReq({token: $routeParams.token, email: $routeParams.email})
       .then(function(){
         $scope.calledApi = true;
         $scope.isActivated = true;
+        $scope.serverBusy = true;
         // redirect to homepage so user can login
         $timeout(function() {
           $location.path('/');
--- ng-app/app/scripts/controllers/main.js
+++ ng-app/app/scripts/controllers/main.js
@@ -16,6 +16,7 @@ angular.module('midwestApp')
       visible: false,
       UserId: null
     };
+    $scope.serverBusy = false;
 
     // redirect to dashboard for logged in users
     if (session.isLoggedIn()) {
@@ -32,9 +33,11 @@ angular.module('midwestApp')
     });
 
     $scope.register = function() {
+      $scope.serverBusy = true;
       $scope.reg.$save(function() {
         $location.path('/confirm_email');
       }, function(resp) {
+        $scope.serverBusy = false;
         $scope.registerErrors = [];
         _.each(resp.data, function(param, errors) {
           $scope.registerErrors.push(errors + ' ' + param);
@@ -43,12 +46,14 @@ angular.module('midwestApp')
     };
 
     $scope.signInUser = function() {
+      $scope.serverBusy = true;
       session.login($scope.signIn.email, $scope.signIn.password)
         .then(function() {
           // clear query param if came from account activation redirect
           $location.url($location.path());
           $location.path('/dashboard');
         }, function(req) {
+          $scope.serverBusy = false;
           $scope.signInError = req.data.message;
           // handle registered user but haven't activate account
           if (req.status === 403 && req.data.user_id !== null) {
@@ -59,6 +64,7 @@ angular.module('midwestApp')
     };
 
     $scope.activateAccount = function() {
+      $scope.serverBusy = true;
       // resend activation email
       $http.get('/api/users/activations/' + $scope.accountActivation.UserId)
         .success(function() {
