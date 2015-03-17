74b62c21366d9e3b4730919e06956005a1c1b718
lidsky <hmr1808@gmail.com>
2015-03-17 13:05:03 -0400

    add overlay/loading gif to navbar on logout

--- ng-app/app/scripts/controllers/navigation.js
+++ ng-app/app/scripts/controllers/navigation.js
@@ -2,10 +2,15 @@
 
 angular.module('midwestApp')
   .controller('NavigationCtrl', function ($scope, $location, session) {
+    $scope.waiting = {
+      logoutResponse: false
+    };
+
     $scope.logout = function() {
+      $scope.waiting.logoutResponse = true;
       session.logout().then(function() {
+        $scope.waiting.logoutResponse = false;
         $location.path('/');
-        console.log('logging out');
       });
     };
 
