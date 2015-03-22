3a02027092f30e32310d34476f88174a433c048c
Sofwan Haddad <sysofwan@umich.edu>
2015-03-22 14:08:19 -0400

    changed migration

--- db/migrate/20150214153539_change_university_id_in_users.rb
+++ db/migrate/20150214153539_change_university_id_in_users.rb
@@ -1,9 +1,11 @@
 class ChangeUniversityIdInUsers < ActiveRecord::Migration
   def up
-    change_column :users, :university, :integer
+    remove_column :users, :university, :string
+    add_column :users, :university, :integer
   end
 
   def down
-    change_column :users, :university, :string
+    remove_column :users, :university, :integer
+    add_column :users, :university, :string
   end
 end
