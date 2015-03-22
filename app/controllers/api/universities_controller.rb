949c06640eb860ef5e7aebaf54ad1e6b33f27a6e
Sofwan Haddad <sysofwan@umich.edu>
2015-03-22 14:30:56 -0400

    added debug statements

--- app/controllers/api/universities_controller.rb
+++ app/controllers/api/universities_controller.rb
@@ -2,16 +2,10 @@ class Api::UniversitiesController < ApplicationController
 	#respond_to :json
 
 	def index
-
 		# Gather all university data
-
 		@universities = University.all
+		puts @universities
 		render json: @universities
-		# Respond to request with university data in json format
-		#respond_with(universities) do |format|
-		#	format.json{ render :json => universities.as_json}
-		#end
-
 	end
 
 	def count
