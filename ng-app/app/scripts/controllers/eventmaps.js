c924c20058e57f11b6d74dbafd059e8a7991a826
Syahril <everaldo@umich.edu>
2015-04-09 22:49:40 -0400

    Added eventmaps.js

--- ng-app/app/scripts/controllers/eventmaps.js
+++ ng-app/app/scripts/controllers/eventmaps.js
@@ -8,6 +8,56 @@
  * Controller of the midwestApp
  */
 
+angular.module('midwestApp')
+  .controller('EventmapsCtrl', function ($scope) {
+    $scope.map = {
+      center: { latitude: 42.2814, longitude : -83.7483},
+      zoom: 12,
+      control: {},
+      routes: {
+        start: [
+          {name: 'Central Campus Recreation Building', address: '401 Washtenaw Avenue, Ann Arbor, Michigan 48109'},
+          {name: 'Mitchell Field', address: '1910 Fuller Road, Ann Arbor, MI 48105'},
+          {name: 'Sports Coliseum', address: '721 S. Fifth Ave, Ann Arbor, MI 48104 '}
+        ],
+        end: [
+          {name: 'Central Campus Recreation Building', address: '401 Washtenaw Avenue, Ann Arbor, Michigan, 48109'},
+          {name: 'Mitchell Field', address: '1910 Fuller Road, Ann Arbor, MI 48105'},
+          {name: 'Sports Coliseum', address: '721 S. Fifth Ave, Ann Arbor, MI 48104'}
+        ]
+      }
+    };
+    $scope.routePoints = {
+      start: {},
+      end: {}
+    };
+    $scope.routePoints.start = $scope.map.routes.start[0];
+    $scope.routePoints.end = $scope.map.routes.end[0];
+    $scope.businesses = [];
+
+    var directionsDisplay = new google.maps.DirectionsRenderer();
+
+    $scope.calcRoute = function (routePoints) {
+      directionsDisplay.setMap($scope.map.control.getGMap());
+      directionsDisplay.setPanel(document.getElementById('panel'));
+      var directionsService = new google.maps.DirectionsService();
+      var start = routePoints.start.address;
+      var end = routePoints.end.address;
+      var request = {
+        origin: start,
+        destination: end,
+        travelMode: google.maps.TravelMode.DRIVING
+      };
+
+      directionsService.route(request, function(response, status) {
+        if (status === google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(response);
+          console.log(response);
+        }
+      });
+      return;
+    };
+  });
 
 
 
