bec94a12b19489b33e6d97618edcae04d21eab96
Eugene Goh <vanblaze5@gmail.com>
2015-04-14 04:10:13 -0400

    Let the commentting sportreg being. Unknown error. I think it is the unpermitted format thingy

--- ng-app/app/scripts/controllers/dashboard.js
+++ ng-app/app/scripts/controllers/dashboard.js
@@ -30,13 +30,13 @@ angular.module('midwestApp')
         });
       }
 
-      if (user.registration_payment_status) {
+      /*if (user.registration_payment_status) {
         $scope.todos.unshift({
           title: 'Register for sports',
           label: 'Coming soon',
           link: 'sportsreg'
         });
-      }
+      }*/
 
       //TODO: Call API to read all games that the player is playing and display it on sports tab
       $http.get('api/participants/get/?user_id=' + user.id)
