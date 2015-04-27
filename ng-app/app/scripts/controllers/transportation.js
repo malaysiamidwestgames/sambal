f4e6e2221f088e0ca7613bc7f7b62e9058e01bbb
Eugene Goh <vanblaze5@gmail.com>
2015-04-27 03:25:00 -0400

    Cleaned up unused jquery code, added scrollspy. Scrollspy still not working, but we have something that works ok now, deploying it.

--- ng-app/app/scripts/controllers/transportation.js
+++ ng-app/app/scripts/controllers/transportation.js
@@ -8,26 +8,6 @@
  * Controller of the midwestApp
  */
 angular.module('midwestApp')
-  .controller('TransportationCtrl', function ($scope) {
-    $(function(){
- 		$(document).on( 'scroll', function(){
- 
-			if ($(window).scrollTop() > 100) {
-				$('.scroll-top-wrapper').addClass('show');
-			} else {
-				$('.scroll-top-wrapper').removeClass('show');
-			}
-		});
- 
-		$('.scroll-top-wrapper').on('click', scrollToTop);
-	});
- 
-	function scrollToTop() {
-		verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
-		element = $('body');
-		offset = element.offset();
-		offsetTop = offset.top;
-		$('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
-	}
-	
+  .controller('TransportationCtrl', function () {
+
   });
