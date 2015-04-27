1a39af6fab5dd393d1cca2209cb17f26f63eff8a
Eugene Goh <vanblaze5@gmail.com>
2015-04-27 06:53:30 -0400

    Remove quantity from products

--- db/migrate/20150427103412_create_products.rb
+++ db/migrate/20150427103412_create_products.rb
@@ -3,7 +3,6 @@ class CreateProducts < ActiveRecord::Migration
     create_table :products do |t|
       t.string :name
       t.string :size
-      t.integer :quantity
 
       t.timestamps null: false
     end
