bdbf6f11a2c8a7e06607628b5af832ee6b43ede8
Eugene Goh <vanblaze5@gmail.com>
2015-05-16 08:44:46 -0400

    Live draw is 80% done, need to add more content

--- ng-app/app/scripts/controllers/livedraw.js
+++ ng-app/app/scripts/controllers/livedraw.js
@@ -9,26 +9,28 @@
  */
 angular.module('midwestApp')
   .controller('LivedrawCtrl', function ($scope, $modal, $http) {
+    var options = {
+      weekday: 'short',
+      month: 'short',
+      day: 'numeric',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric'
+    };
 
     $scope.selectedSport = {};
     $http.get('/api/games')
       .success(function(data){
         $scope.games = data.games;
-        //console.log($scope.games);
         $scope.games.forEach(function(game) {
           game.link = 'assets/MMG2015_' + game.name + '.pdf';
+          if (game.live_draw_session) {
+            var session = new Date(game.live_draw_session);
+            game.live_draw_session = session.toLocaleString('en-us', options);
+          } else {
+            game.live_draw_session = 'No Draws';
+          }
         });
-        /*$scope.sports = [{
-          id: 0,
-          name: 'General Rules'
-        }];
-        data.sports.forEach(function(entry){
-          $scope.sports.push(entry);
-        });
-        $scope.sports.forEach(function(entry) {
-          entry.link = 'assets/MMG2015_' + entry.name + '.pdf';
-        });
-        $scope.selectedSport = $scope.sports[0];*/
       });
 
     $scope.open = function (game) {
