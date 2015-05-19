919f47570d6e302f716a3fc2e88351259ebe8870
Eugene Goh <vanblaze5@gmail.com>
2015-05-19 04:29:30 -0400

    Live draw est

--- ng-app/app/scripts/controllers/livedraw.js
+++ ng-app/app/scripts/controllers/livedraw.js
@@ -27,7 +27,7 @@ angular.module('midwestApp')
         $scope.games.forEach(function(game) {
           game.link = 'assets/MMG2015_' + game.name + '.pdf';
           if (game.live_draw_session) {
-            var session = moment(game.live_draw_session).tz('America/Los_Angelue').format('ddd MMM D YYYY, h:mm a');
+            var session = moment(game.live_draw_session).tz('America/Los_Angeles').format('ddd MMM D YYYY, h:mm a');
             console.log(session);
             game.live_draw_session = session.toLocaleString('en-us', options);
           } else {
