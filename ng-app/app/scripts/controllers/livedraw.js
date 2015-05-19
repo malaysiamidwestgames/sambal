685cea5c58139cc7d1c5f64dbd3c7fa1ca6cdbc1
Eugene Goh <vanblaze5@gmail.com>
2015-05-19 04:40:40 -0400

    Remove console logs

--- ng-app/app/scripts/controllers/livedraw.js
+++ ng-app/app/scripts/controllers/livedraw.js
@@ -27,9 +27,7 @@ angular.module('midwestApp')
         $scope.games.forEach(function(game) {
           game.link = 'assets/MMG2015_' + game.name + '.pdf';
           if (game.live_draw_session) {
-            console.log('ori: ' + game.live_draw_session);
             var session = moment(game.live_draw_session).tz('America/New_York').format('ddd MMM D YYYY, h:mm a');
-            console.log('new: ' + session);
             game.live_draw_session = session.toLocaleString('en-us', options);
           } else {
             game.live_draw_session = 'No Draws';
