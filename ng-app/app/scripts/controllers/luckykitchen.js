e651d9b5562b90687ee48396fc356f57a6cbe47e
Eugene Goh <vanblaze5@gmail.com>
2015-05-21 18:25:22 -0400

    Updated lucky kitchen

---
+++ ng-app/app/scripts/controllers/luckykitchen.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name midwestApp.controller:LuckykitchenCtrl
+ * @description
+ * # LuckykitchenCtrl
+ * Controller of the midwestApp
+ */
+angular.module('midwestApp')
+  .controller('LuckykitchenCtrl', function ($scope) {
+    $scope.myInterval = 5000;
+    $scope.slides = [];
+    $scope.slides.push({
+      image: 'images/LK_Spring Rolls.jpg',
+      text: 'Yummy Lucky Kitchen Spring Rolls'
+    });
+    $scope.slides.push({
+      image: 'images/LK_Homestyle Tofu.jpg',
+      text: 'Homestyle Tofuuuuuuuuuuuuu!'
+    });
+    $scope.slides.push({
+      image: 'images/LK_Dumplings.jpg',
+      text: 'How do you say no this these awesome Lucky Kitchen Dumplings?'
+    });
+    $scope.slides.push({
+      image: 'images/LK_Gen Tso\'s Chicken.jpg',
+      text: 'General Tso\'s Chicken'
+    });
+    $scope.slides.push({
+      image: 'images/LK_Tilapia Fish in Brown Sauce.jpg',
+      text: 'Best dish in Lucky Kitchen'
+    });
+  });
