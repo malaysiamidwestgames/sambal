936545ce0b7537834e9a5daf9053a94f78b96134
Eugene Goh <vanblaze5@gmail.com>
2015-05-31 00:21:28 -0700

    Updating landing page

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,5 +3,17 @@ $(function() {
 	var todayDate = new Date();
 
 	var diffInDay = Math.floor((midwestDate - todayDate)/(1000 * 60 * 60 * 24));
-	$('.countdown .number').text(diffInDay);
+	var topText = diffInDay;
+	var botText = 'days to go';
+
+	if (diffInDay <= 0 && diffInDay >= -2) {
+		topText = 'Event happening now!';
+		botText = 'Are you here?!';
+	} else if (diffInDay < -2) {
+		topText = 'Who will be the host of Malaysia Midwest Games 2016?';
+		botText = 'Check out the winners of MMG 2015 now!'
+	}
+
+	$('.countdown .number').text(topText);
+	$('.countdown .desc').text(botText);
 });
