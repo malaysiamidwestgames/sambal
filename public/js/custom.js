65855e7fbeae5c5147f748652fe771972efef8e4
Eugene Goh <vanblaze5@gmail.com>
2015-05-31 00:51:50 -0700

    Fixed landing page

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,6 +1,6 @@
 $(function() {
-	var midwest2015Date = new Date(2015, 5, 22);
-	var midwest2016Date = new Date(2016, 5, 27);
+	var midwest2015Date = new Date(2015, 4, 22);
+	var midwest2016Date = new Date(2016, 4, 27);
 	var todayDate = new Date();
 
 	var diffInDay = Math.floor((midwest2015Date - todayDate)/(1000 * 60 * 60 * 24));
@@ -12,8 +12,8 @@ $(function() {
 		topText = 'Event happening now!';
 		botText = 'Are you here?!';
 	} else if (diffInDay < -2) {
-		topText = futureDiff;
-		botText = 'days to go\nCheck out the winners of MMG 2015 now!'
+		topText = futureDiff.toString();
+		botText = 'days to MMG2016'
 	}
 
 	$('.countdown .number').text(topText);
